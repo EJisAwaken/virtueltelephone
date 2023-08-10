30f58cfe1ece5527c3ac77ab82cc1d5489dd72d8
EJisAwaken <fenoasy@yahoo.com>
2023-08-11 00:43:20 +0300

    test

---
+++ script.js
@@ -0,0 +1,53 @@
+let clavierColor = document.querySelector("#marque");
+let clavier = document.querySelector("#clavier");
+let torche = document.querySelector("#torche");
+let zero = document.querySelector("#zero");
+let lumiere = document.querySelector("#lumiere");
+
+clavierColor.addEventListener("click", ()=>{
+    clavier.style.backgroundColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+})
+
+zero.addEventListener("dblclick", ()=>{
+    torche.style.backgroundColor = "yellow";
+    lumiere.style.display = "block";
+
+})
+
+zero.addEventListener("click", ()=>{
+    torche.style.backgroundColor = "white";
+    lumiere.style.display = "none";
+
+})
+
+
+// js de la deuxieme programe
+
+let moment = new Date();
+let hours = moment.getHours();
+let minutes = moment.getMinutes();
+let days = moment.getDay();
+let month = moment.getMonth();
+let years = moment.getFullYear();
+
+let temps = document.querySelector("#temps");
+let date = document.querySelector("#date");
+
+window.addEventListener("load", ()=>{
+    temps.innerText = hours + " : " + minutes
+    date.innerText = days +  " / " + month + " / " + years;
+})
+
+
+let refus = document.querySelector("#refuser");
+let demare = document.querySelector("#demarrage");
+let accueil = document.querySelector("#Accueil");
+
+refus.addEventListener("click", function () {
+        demare.style.display ="block";
+    setTimeout(function () {
+        demare.style.display ="none";
+        accueil.style.display ="block";
+    }, 5000);
+})
+
